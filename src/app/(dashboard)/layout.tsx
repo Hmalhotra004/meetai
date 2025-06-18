0e74d104001f2ec10b36282737e36b6b37215920
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-06-18 21:35:56 +0530

    sidebar

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,18 @@
+import DashboardSidebar from "@/components/DashboardSidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import React from "react";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const layout = ({ children }: LayoutProps) => {
+  return (
+    <SidebarProvider>
+      <DashboardSidebar />
+      <main className="flex flex-col h-screen w-screen bg-muted">{children}</main>
+    </SidebarProvider>
+  );
+};
+
+export default layout;
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,25 +1,9 @@
 "use client";
-import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
 
 const Home = () => {
   const { data } = authClient.useSession();
-  const router = useRouter();
-  return (
-    <div>
-      {data?.user.name}
-      <Button
-        onClick={() =>
-          authClient.signOut({
-            fetchOptions: { onSuccess: () => router.replace("/sign-in") },
-          })
-        }
-      >
-        out
-      </Button>
-    </div>
-  );
+  return <div>{data?.user.name}</div>;
 };
 
 export default Home;
