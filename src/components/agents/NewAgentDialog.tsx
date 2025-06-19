1ab7c8ef2f353141328920aa8079323a5b0398ca
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-06-20 00:20:31 +0530

    create agent

---
+++ src/components/agents/NewAgentDialog.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import ResponsiveDialog from "../ResponsiveDialog";
+import AgentForm from "./AgentForm";
+
+interface NewAgentDialogProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+const NewAgentDialog = ({ open, onOpenChange }: NewAgentDialogProps) => {
+  return (
+    <ResponsiveDialog
+      open={open}
+      onOpenChange={onOpenChange}
+      title="New Agent"
+      description="Create a new agent."
+    >
+      <AgentForm
+        onSuccess={() => onOpenChange(false)}
+        onCancel={() => onOpenChange(false)}
+      />
+    </ResponsiveDialog>
+  );
+};
+
+export default NewAgentDialog;
--- src/procedures/agent.ts
+++ src/procedures/agent.ts
@@ -1,11 +1,36 @@
 import db from "@/db";
 import { agents } from "@/db/schema";
-import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+import { agentsInsertSchema } from "./schemas";
 
 export const agentsRouter = createTRPCRouter({
-  getMany: baseProcedure.query(async () => {
-    const data = db.select().from(agents);
+  getMany: protectedProcedure.query(async () => {
+    const data = await db.select().from(agents);
 
     return data;
   }),
+
+  getOne: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input }) => {
+      const [existingAgent] = await db
+        .select()
+        .from(agents)
+        .where(eq(agents.id, input.id));
+
+      return existingAgent;
+    }),
+
+  create: protectedProcedure
+    .input(agentsInsertSchema)
+    .mutation(async ({ input, ctx }) => {
+      const [createdAgent] = await db
+        .insert(agents)
+        .values({ ...input, userId: ctx.auth.user.id })
+        .returning();
+
+      return createdAgent;
+    }),
 });
