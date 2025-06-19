4402cbc7280cbf0b7d08dd3817461a6aa058fada
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-06-19 15:37:04 +0530

    trpc

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,9 +1,12 @@
 "use client";
-import { authClient } from "@/lib/auth-client";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 const Home = () => {
-  const { data } = authClient.useSession();
-  return <div>{data?.user.name}</div>;
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({ text: "balle balle" }));
+
+  return <div>{data?.greeting}</div>;
 };
 
 export default Home;
